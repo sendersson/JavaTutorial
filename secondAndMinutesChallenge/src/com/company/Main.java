793bef05eea1aa632c469e3c41211e5a4eb98885
William <william@skoglycke.se>
2018-08-17 18:11:17 +0200

    Update
    dsadsa

--- secondAndMinutesChallenge/src/com/company/Main.java
+++ secondAndMinutesChallenge/src/com/company/Main.java
@@ -2,47 +2,50 @@ package com.company;
 
 public class Main {
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
     public static void main(String[] args) {
 
-        System.out.println(getDurationString(0,1));
-        System.out.println(getDurationString(70000));
+        System.out.println(getDurationString(65,9));
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(-41));
 
     }
 
-    public static String getDurationString(int minutes,int seconds){
+    private static String getDurationString(int minutes, int seconds){
 
-        if(minutes < 0 || seconds >=60 || seconds <0){return "Invalid value";}
+        if((minutes < 0) || (seconds > 59) || (seconds < 0)){
+            return INVALID_VALUE_MESSAGE;
+        }
 
         int hours = minutes/60;
         int remainingMinutes = minutes % 60;
 
-        if(hours < 10 && remainingMinutes < 10){
-            return ("0"+hours +"h 0" +remainingMinutes +"m " +seconds +"s");
-        }
-        else if(hours < 10){
-            return ("0"+hours +"h " +remainingMinutes +"m " +seconds +"s");
-        }
-        else if(remainingMinutes < 10){
-            return (hours +"h 0" +remainingMinutes +"m " +seconds +"s");
-        }
+        String hoursString = hours +"h ";
+        String minutesString = remainingMinutes +"m ";
+        String secondsString = seconds +"s";
 
-        return (+hours +"h " +remainingMinutes +"m " +seconds +"s");
-
+        if(hours < 10){
+            hoursString = "0" +hoursString;
+        }
+        if(remainingMinutes < 10){
+            minutesString = "0" +minutesString;
+        }
+        if(seconds < 10){
+            secondsString = "0" +secondsString;
+        }
+        return hoursString + minutesString + secondsString;
     }
 
-    public static String getDurationString(int seconds){
+    private static String getDurationString(int seconds){
 
         if(seconds <0){
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         int minutes = seconds / 60;
         int remainingSeconds = seconds % 60;
 
-        int hours = minutes / 60;
-        int remainingMinutes = minutes % 60;
-
-        return (hours +"h " +remainingMinutes +"m " +remainingSeconds +"s");
+        return getDurationString(minutes,remainingSeconds);
 
     }
 }
