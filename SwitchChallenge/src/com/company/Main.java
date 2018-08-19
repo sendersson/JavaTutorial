aa37897da0ed9845d5ea66dc4dc1d41199f0e785
William <william@skoglycke.se>
2018-08-19 22:38:17 +0200

    19/8 22.38

--- SwitchChallenge/src/com/company/Main.java
+++ SwitchChallenge/src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        printDayOfTheWeek(1);
+
         char switchValue = 'f';
 
         switch (switchValue) {
@@ -42,4 +44,39 @@ public class Main {
         }
 
     }
-}
+
+        public static void printDayOfTheWeek(int day){
+
+            switch (day){
+
+                case 1:
+                    System.out.println("monday");
+                    break;
+                case 2:
+                    System.out.println(day);
+                    break;
+                case 3:
+                    System.out.println(day);
+                    break;
+                case 4:
+                    System.out.println(day);
+                    break;
+                case 5:
+                    System.out.println(day);
+                    break;
+                case 6:
+                    System.out.println(day);
+                    break;
+                case 7:
+                    System.out.println(day);
+                    break;
+                default:
+                    System.out.println("Invalid value");
+
+            }
+
+        }
+
+    }
+
+
